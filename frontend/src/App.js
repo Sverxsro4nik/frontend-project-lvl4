601babcc8ef0922f2cf9f9d44f51f326f0dc6a61
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-07-14 20:54:51 +0500

    delete standart files, load react bootstrap and axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login.jsx';
 import NotFound from './components/NotFound.jsx';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/'>
-
-        </Route>
+        <Route exact path='/'></Route>
         <Route path='/login' element={<Login />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
