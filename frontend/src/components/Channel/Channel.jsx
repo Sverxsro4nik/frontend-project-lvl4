4800a6745ca844bd10e7934fe2170218df85d2c6
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-07-24 17:23:04 +0500

    update channels style

--- frontend/src/components/Channel/Channel.jsx
+++ frontend/src/components/Channel/Channel.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 
 const Channel = (props) => {
-  const { name } = props;
+  const { name, defaultActiveChannel, channelId } = props;
+  const variant = channelId === defaultActiveChannel ? 'secondary' : 'light';
   return (
     <>
-      <Button variant='secondary' className='w-100 rounded-0 text-start'>
+      <Button variant={variant} className='w-100 rounded-0 text-start'>
         <span className='me-1'>#</span>
         {name}
       </Button>
