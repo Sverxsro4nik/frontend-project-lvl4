71fd15c53aaf737c63cbbc053aef0740205a15a5
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-07-25 23:04:40 +0500

    add first socket

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { io } from 'socket.io-client';
+
 import AuthProvider from './context/AuthProvider.jsx';
 
 import MainPage from './MainPage.jsx';
+import { addMessage } from './slices/messagesSlice.js';
 
-function App() {
+const App = () => {
+  const socket = io();
+  const dispath = useDispatch();
+
+  socket.on("newMessage", ({ message }) => {
+    dispath(addMessage(message));
+  });
+
+  socket.emit('newMessage', ({ message }) => dispath(addMessage(message)));
   return (
     <AuthProvider>
       <div className='h-100' id='chat'>
