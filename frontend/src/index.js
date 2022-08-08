aa8a6390ee3f4f6cd722134ceec4548ac686c306
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-08-08 20:59:31 +0500

    change name from App to init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './init.js';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 
